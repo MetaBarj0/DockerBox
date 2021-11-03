2d0a2d581dfec660f398130d77642a4382eccb30
MetaBarj0 <troctsch.cpp@gmail.com>
2021-11-03 17:52:45 +0100

    refacto
    - Vagrantfile massive refacto
    - extended testing is still necessary

--- Vagrantfile
+++ Vagrantfile
@@ -1,74 +1,60 @@
-Vagrant.require_version ">= 2.2.0"
-VAGRANTFILE_API_VERSION = "2"
+Vagrant.require_version '>= 2.2.0'
+VAGRANTFILE_API_VERSION = '2'
+BOX_NAME = 'metabarj0/DockerBox'
+REQUIRED_BOX_VERSION = '>= 3.0.0'
 
 require './modules/dockerbox'
 
-project = DockerBox::VagrantProject.new( 'config.yaml' )
+Vagrant.configure( VAGRANTFILE_API_VERSION ) do | config |
+  project = DockerBox::VagrantProject.new( 'config.yaml' )
 
-
-
-
-
-
-
-
-
-
-Vagrant.configure( "2" ) do | config |
-  multi_machines_vm_prefix_builder = DockerBox::MultiMachineVmPrefixBuilder.new( 'config.yaml' )
-  multi_machines_hostname_builder = DockerBox::MultiMachineHostnameBuilder.new( 'config.yaml' )
-
-  # creating machines
-  DockerBox::get_machines_ip_addresses( 'config.yaml' ).each_with_index do | ip, multi_machine_index |
+  project.from_machines_ip_addresses.each_with_index do | ip, machine_index |
     config.ssh.username = "docker"
-    config.ssh.extra_args = DockerBox::get_machine_ssh_command_extra_args( 'config.yaml' )
+    config.ssh.extra_args = project.get_machine_ssh_command_extra_args()
 
-    config.vm.define "#{ multi_machines_vm_prefix_builder.get_next_vm_prefix( multi_machine_index ) }" do | machine |
-      machine.vm.box = "metabarj0/DockerBox"
-      machine.vm.box_version = ">= 3.0.0"
-      machine.vm.hostname = multi_machines_hostname_builder.get_next_hostname( multi_machine_index )
+    config.vm.define "#{ project.get_next_machine_name_prefix( machine_index ) }" do | machine |
+      machine.vm.box = BOX_NAME
+      machine.vm.box_version = REQUIRED_BOX_VERSION
+      machine.vm.hostname = project.get_next_machine_hostname( machine_index )
 
-      if DockerBox::has_public_network( 'config.yaml', multi_machine_index )
+      if project.has_setup_public_network_for_machine( machine_index )
         machine.vm.network "public_network"
       end
 
-      DockerBox::get_forwarded_ports( 'config.yaml', multi_machine_index ).each { | rule |
+      project.from_machine_forwarded_ports( machine_index ).each { | rule |
         machine.vm.network "forwarded_port", guest: rule[ 'guest' ], host: rule[ 'host' ], protocol: rule[ 'protocol' ]
       }
 
-      DockerBox::get_synced_folders( 'config.yaml', multi_machine_index ).each { | synced_folder |
+      project.from_machine_synced_folders( machine_index ).each { | synced_folder |
         machine.vm.synced_folder synced_folder[ 'host' ], synced_folder[ 'guest' ]
       }
 
-      if DockerBox::is_multi_machine_enabled( 'config.yaml' )
+      if project.is_multi_machine_enabled()
         machine.vm.network "private_network", ip: "#{ ip }"
       end
 
-      # virtualbox provider specific configuration with defaults
       machine.vm.provider "virtualbox" do | v |
-        v.customize [ "modifyvm", :id, "--cpus", DockerBox::get_machine_cpu_count( 'config.yaml', multi_machine_index ) ]
-        v.customize [ "modifyvm", :id, "--cpuexecutioncap", DockerBox::get_machine_cpu_cap( 'config.yaml', multi_machine_index ) ]
-        v.customize [ "modifyvm", :id, "--memory", DockerBox::get_machine_memory( 'config.yaml', multi_machine_index ) ]
+        v.customize [ "modifyvm", :id, "--cpus", project.get_machine_cpu_count( machine_index ) ]
+        v.customize [ "modifyvm", :id, "--cpuexecutioncap", project.get_machine_cpu_cap( machine_index ) ]
+        v.customize [ "modifyvm", :id, "--memory", project.get_machine_memory( machine_index ) ]
       end
 
-      # shell provisioning
       machine.vm.provision "shell", path: "provisioning/provision-from-host.sh",
                                     env:
                                     {
                                       "MACHINE_HOSTNAME"          => machine.vm.hostname,
-                                      "ZONEINFO_REGION"           => DockerBox::get_provision_properties( 'config.yaml' ).zoneinfo_region,
-                                      "ZONEINFO_CITY"             => DockerBox::get_provision_properties( 'config.yaml' ).zoneinfo_city,
-                                      "KEYMAP"                    => DockerBox::get_provision_properties( 'config.yaml' ).keymap,
-                                      "KEYMAP_VARIANT"            => DockerBox::get_provision_properties( 'config.yaml' ).keymap_variant,
-                                      "EXTRA_PACKAGES"            => DockerBox::get_provision_properties( 'config.yaml' ).extra_packages,
-                                      "DOCKER_VOLUME_AUTO_EXTEND" => DockerBox::get_provision_properties( 'config.yaml' ).docker_volume_auto_extend,
-                                      "KV_DB_FILE"                => DockerBox::get_provision_properties( 'config.yaml' ).kv_db_file,
-                                      "KV_DB_FILE_CREATE_LINK"    => DockerBox::get_provision_properties( 'config.yaml' ).kv_db_file_create_link,
-                                      "KV_RECORD_SEPARATOR"       => DockerBox::get_provision_properties( 'config.yaml' ).kv_record_separator,
-                                      "KV_ASSIGNMENT_OPERATOR"    => DockerBox::get_provision_properties( 'config.yaml' ).kv_assignment_operator,
-                                      "KV_DB_RECORDS"             => DockerBox::get_provision_properties( 'config.yaml' ).kv_db_records
+                                      "ZONEINFO_REGION"           => project.provisioning_properties().zoneinfo_region,
+                                      "ZONEINFO_CITY"             => project.provisioning_properties().zoneinfo_city,
+                                      "KEYMAP"                    => project.provisioning_properties().keymap,
+                                      "KEYMAP_VARIANT"            => project.provisioning_properties().keymap_variant,
+                                      "EXTRA_PACKAGES"            => project.provisioning_properties().extra_packages,
+                                      "DOCKER_VOLUME_AUTO_EXTEND" => project.provisioning_properties().docker_volume_auto_extend,
+                                      "KV_DB_FILE"                => project.provisioning_properties().kv_db_file,
+                                      "KV_DB_FILE_CREATE_LINK"    => project.provisioning_properties().kv_db_file_create_link,
+                                      "KV_RECORD_SEPARATOR"       => project.provisioning_properties().kv_record_separator,
+                                      "KV_ASSIGNMENT_OPERATOR"    => project.provisioning_properties().kv_assignment_operator,
+                                      "KV_DB_RECORDS"             => project.provisioning_properties().kv_db_records
                                     }
-
     end
   end
 end
--- modules/dockerbox.rb
+++ modules/dockerbox.rb
@@ -9,6 +9,141 @@ module DockerBox
       install_specified_plugins( %w( vagrant-vbguest ) )
       install_extra_plugins_from_configuration()
       setup_vagrant_provider_from_configuration()
+
+      @multi_machine = get_multi_machine_properties()
+      @single_machine = get_single_machine_properties()
+
+      @multi_machine_name_prefix_map = {}
+      @multi_machines_hostname_prefix_map = {}
+    end
+
+    def from_machines_ip_addresses()
+      return @multi_machine.ip_addresses.select{ | ip | ip }
+    end
+
+    def get_machine_ssh_command_extra_args()
+      return @single_machine.ssh_command_extra_args.dup
+    end
+
+    # TODO: dirty function, referential transparency is violated
+    def get_next_machine_name_prefix( index )
+      if not is_multi_machine_enabled()
+        return 'default' 
+      end
+
+      vm_prefix = @multi_machine.vm_prefixes[ index ]
+
+      if ( vm_prefix == nil ) || ( vm_prefix.empty? )
+        vm_prefix = 'machine'
+      end
+
+      if @multi_machine_name_prefix_map.include?( vm_prefix )
+        @multi_machine_name_prefix_map[ vm_prefix ] = @multi_machine_name_prefix_map[ vm_prefix ] + 1
+      else
+        @multi_machine_name_prefix_map[ vm_prefix ] = 0
+      end
+
+      return "#{ vm_prefix }-#{ @multi_machine_name_prefix_map[ vm_prefix ] }"
+    end
+
+    # TODO: dirty function, referential transparency is violated
+    def get_next_machine_hostname( index )
+
+      if not is_multi_machine_enabled()
+        return @single_machine.hostname
+      end
+
+      hostname_prefix = @multi_machine.hostname_prefixes[ index ]
+
+      if ( hostname_prefix == nil ) || hostname_prefix.empty?
+        hostname_prefix = @single_machine.hostname
+      end
+
+      if @multi_machines_hostname_prefix_map.include?( hostname_prefix )
+        @multi_machines_hostname_prefix_map[ hostname_prefix ] = @multi_machines_hostname_prefix_map[ hostname_prefix ] + 1
+      else
+        @multi_machines_hostname_prefix_map[ hostname_prefix ] = 0
+      end
+
+      return "#{ hostname_prefix }-#{ @multi_machines_hostname_prefix_map[ hostname_prefix ] }"
+    end
+
+    def has_setup_public_network_for_machine( index )
+      # vagrant bug : not supported but should work as soon as vagrant has fixed its stuff
+      is_single_machine_public_network_enabled = @single_machine.create_public_network && ( not is_multi_machine_enabled() )
+      is_multi_machine_public_network_enabled = @multi_machine.create_public_network[ index ]
+
+      return is_single_machine_public_network_enabled || is_multi_machine_public_network_enabled
+    end
+
+    def from_machine_forwarded_ports( index )
+      return can_setup_forwarded_ports_for_machine( index ) ? single_machine.forwarded_ports.select { | rule | rule } : []
+    end
+
+    def from_machine_synced_folders( index )
+      if not @single_machine.synced_folders
+        return []
+      end
+
+      if ( not is_multi_machine_enabled ) || multi_machine_have_shared_synced_folders( index )
+        return @single_machine.synced_folders.select { | synced_folder | synced_folder }
+      end
+
+      return []
+    end
+
+    def is_multi_machine_enabled()
+      return @multi_machine.ip_addresses.length() > 1
+    end
+
+    def get_machine_cpu_count( index )
+      if not is_multi_machine_enabled()
+        return @single_machine.cpu
+      end
+
+      if @multi_machine.cpus[ index ] && ( @multi_machine.cpus[ index ] > 0 )
+        return @multi_machine.cpus[ index ]
+      end
+    end
+
+    def get_machine_cpu_cap( index )
+      if not is_multi_machine_enabled()
+        return @single_machine.cpu_cap
+      end
+
+      if @multi_machine.cpu_caps[ index ] && ( @multi_machine.cpu_caps[ index ] > 0 )
+        return @multi_machine.cpu_caps[ index ]
+      end
+    end
+
+    def get_machine_memory( index )
+      if not is_multi_machine_enabled()
+        return @single_machine.memory
+      end
+
+      if @multi_machine.memories[ index ] && ( @multi_machine.memories[ index ] > 0 )
+        return @multi_machine.memories[ index ]
+      end
+    end
+
+    def provisioning_properties()
+      return Class.new do
+        def initialize( configuration )
+          @configuration = configuration
+        end
+
+        def zoneinfo_region()           @configuration[ 'provisioning' ][ 'zoneinfo_region' ].dup() end;
+        def zoneinfo_city()             @configuration[ 'provisioning' ][ 'zoneinfo_city' ].dup() end;
+        def keymap()                    @configuration[ 'provisioning' ][ 'keymap' ].dup() end;
+        def keymap_variant()            @configuration[ 'provisioning' ][ 'keymap_variant' ].dup() end;
+        def docker_volume_auto_extend() @configuration[ 'provisioning' ][ 'docker_volume_auto_extend' ] ? 1 : 0 end;
+        def extra_packages()            ( defined? @configuration[ 'provisioning' ][ 'extra_packages' ].join ) ? @configuration[ 'provisioning' ][ 'extra_packages' ].join(' ') : '' end;
+        def kv_db_file()                @configuration[ 'provisioning' ][ 'kv_db_file' ].dup() end;
+        def kv_db_file_create_link()    @configuration[ 'provisioning' ][ 'kv_db_file_create_link ' ] ? 1 : 0 end;
+        def kv_record_separator()       @configuration[ 'provisioning' ][ 'kv_record_separator' ].dup() end;
+        def kv_assignment_operator()    @configuration[ 'provisioning' ][ 'kv_assignment_operator' ].dup() end;
+        def kv_db_records()             ( defined? @configuration[ 'provisioning' ][ 'kv_db_records' ].join ) ? @configuration[ 'provisioning' ][ 'kv_db_records' ].join( kv_record_separator ) : '' end;
+      end.new( @configuration )
     end
 
   private
@@ -65,6 +200,52 @@ module DockerBox
       end
 
       ENV[ 'VAGRANT_DEFAULT_PROVIDER' ] = vagrant_provider
+    end
+
+    def get_multi_machine_properties()
+      return Class.new do
+        def initialize( configuration )
+          @configuration = configuration
+        end
+
+        def ip_addresses()           @configuration[ 'multi_machine' ][ 'ip_addresses' ] || [ '' ] end;
+        def create_public_network()  @configuration[ 'multi_machine' ][ 'create_public_network' ] || [] end;
+        def shared_synced_folders()  @configuration[ 'multi_machine' ][ 'shared_synced_folders' ] || [] end;
+        def vm_prefixes()            @configuration[ 'multi_machine' ][ 'vm_prefixes' ] || [] end;
+        def hostname_prefixes()      @configuration[ 'multi_machine' ][ 'hostname_prefixes' ] || [] end;
+        def cpus()                   @configuration[ 'multi_machine' ][ 'cpus' ] end;
+        def cpu_caps()               @configuration[ 'multi_machine' ][ 'cpu_caps' ] end;
+        def memories()               @configuration[ 'multi_machine' ][ 'memories' ] end;
+      end.new( @configuration )
+    end
+
+    def get_single_machine_properties()
+      return Class.new do
+        def initialize( configuration )
+          @configuration = configuration
+        end
+
+        def hostname()               @configuration[ 'single_machine' ][ 'hostname' ] end;
+        def cpu()                    @configuration[ 'single_machine' ][ 'cpu' ] end;
+        def cpu_cap()                @configuration[ 'single_machine' ][ 'cpu_cap' ] end;
+        def memory()                 @configuration[ 'single_machine' ][ 'memory' ] end;
+        def create_public_network()  @configuration[ 'single_machine' ][ 'create_public_network' ] end;
+        def forwarded_ports()        @configuration[ 'single_machine' ][ 'forwarded_ports' ] end;
+        def synced_folders()         @configuration[ 'single_machine' ][ 'synced_folders' ] end;
+        def ssh_command_extra_args() @configuration[ 'single_machine' ][ 'ssh_command_extra_args' ] end;
+      end.new( @configuration )
+    end
+
+    def can_setup_forwarded_ports_for_machine( index )
+      if index > 0
+        return false
+      end
+
+      return @single_machine.forwarded_ports && @single_machine.forwarded_ports.length() > 0
+    end
+
+    def multi_machine_have_shared_synced_folders( index )
+      return @multi_machine.shared_synced_folders[ index ]
     end
   end
 
